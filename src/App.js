094dacb894a9e25ee88db9d34319fc16a77d468c
anymex <anyback@iesfrosur.edu.mx>
2020-08-21 17:43:32 -0500

    borrar estado inecesario

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@
 import React, { Component } from 'react';
 
 import Navbar from './components/Navbar';
-import Content from './components/Content';
+import CreateNota from './components/CreateNota';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date() };
-    }
-
     render() {
         return (
             <>
                 <Navbar />
-                <Content />
+                <CreateNota />
             </>
         );
     }
